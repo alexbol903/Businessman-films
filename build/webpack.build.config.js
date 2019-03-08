37ef449637e22f54bd80ed91add685b72025714f
alexbol903 <42297099+alexbol903@users.noreply.github.com>
2019-03-08 21:30:52 +0300

    Version 2 via webpack

---
+++ build/webpack.build.config.js
@@ -0,0 +1,13 @@
+// Mode building
+
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.config');
+
+const buildWebpackConfig = merge(baseWebpackConfig, {
+	mode: 'production',
+	plugins: []
+});
+
+module.exports = new Promise((resolve, reject) => {
+	resolve(buildWebpackConfig);
+});
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './js/video';
+import './js/animate';
+
+import './scss/main.scss';
