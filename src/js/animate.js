56e9073d37aff06f6bcf71a428527bec28f929d5
alexbol903 <42297099+alexbol903@users.noreply.github.com>
2019-03-10 00:13:44 +0300

    add smooth scroll

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,8 +1,20 @@
 import sal from 'sal.js';
+import jump from 'jump.js';
 
+// Animate blocks 
 sal({
 	threshold: 0.5,
   once: true
 });
 
+// Smooth scroll
+const btnSkills = document.querySelector('.btnSkills');
+const btnPrice = document.querySelectorAll('.btnPrice');
+const btnProjects = document.querySelectorAll('.btnProjects');
+const btnContacts = document.querySelector('.btnContacts');
+
+btnSkills.addEventListener('click', () => jump('#skills', { offset: 60 }));
+btnProjects.forEach(el => el.addEventListener('click', () => jump('#projects', { offset: 60 })));
+btnPrice.forEach(el => el.addEventListener('click', () => jump('#price', {offset: 60})));
+btnContacts.addEventListener('click', () => jump('#contacts', { offset: 60 }));
 
