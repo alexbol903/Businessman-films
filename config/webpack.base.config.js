bc6a08cea00fd841a97ce3dccda8d37d0dbaf793
alexbol903 <42297099+alexbol903@users.noreply.github.com>
2019-03-16 21:53:19 +0300

    Update

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -9,7 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const PATHS = {
 	src: path.join(__dirname, '../src'),
 	dist: path.join(__dirname, '../dist'),
-	assets: 'assets/'
+	assets: './assets/'
 };
 
 module.exports = {
@@ -23,7 +23,7 @@ module.exports = {
 	output: {
 		filename: `${PATHS.assets}js/[name].js`,
 		path: PATHS.dist,
-		publicPath: '/'
+		publicPath: ''
 	},
 
 	module: {
